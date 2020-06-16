ef7c69d30bcd1d571187b135561fa204ca97fd13
Austin Chappell <achappell@triumphpay.com>
2020-06-16 08:46:11 -0500

    improve auth

--- server/middlewares/csrf.js
+++ server/middlewares/csrf.js
@@ -1,10 +1,23 @@
-async function csrfMiddleware(req, res, next) {
-  const csrfCookie = req.cookies.csrf_token;
-  const csrfToken = req.headers['x-csrf-token'];
+const jwt = require('jsonwebtoken');
 
-  if (csrfCookie === csrfToken) {
-    next();
-  } else {
+async function csrfMiddleware(req, res, next) {
+  const csrfHeader = req.headers['x-csrf-token'];
+
+  try {
+    const token = req.cookies && req.cookies.token;
+
+    if (token) {
+      const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+      const { csrfToken } = decoded;
+
+      if (csrfToken === csrfHeader) {
+        return next();
+      }
+    }
+
+    throw new Error();
+  } catch (error) {
     res.status(401).json({ message: 'Unauthorized' });
   }
 }
